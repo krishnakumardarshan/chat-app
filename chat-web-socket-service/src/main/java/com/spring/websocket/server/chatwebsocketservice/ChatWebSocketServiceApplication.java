d874d6e093209e21e33867f8d66564f12d105c37
darshan.k <krishnakumar.darshan@googlemail.com>
2025-11-23 21:09:38 +0800

    initial commit

---
+++ chat-web-socket-service/src/main/java/com/spring/websocket/server/chatwebsocketservice/ChatWebSocketServiceApplication.java
@@ -0,0 +1,13 @@
+package com.spring.websocket.server.chatwebsocketservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatWebSocketServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatWebSocketServiceApplication.class, args);
+    }
+
+}
